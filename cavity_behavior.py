cc805c296c52857a0bdfd4622c1c428c3a0b73a6
PIERRE BEDOUCHA <xbyska@fi.muni.cz>
2020-02-07 13:10:51 +0100

    Use of fill between for positive and negative difference only

--- cavity_behavior.py
+++ cavity_behavior.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import math
 from scipy.spatial import distance
 import numpy as np
+import scipy
 import Bio.PDB
 import Bio.AlignIO as al
 from sklearn import preprocessing
@@ -14,7 +15,40 @@ import csv
 def facet_scatter(x, y, **kwargs):
     """Draw scatterplot with point colors from a faceted DataFrame columns."""
     kwargs.pop("color")
-    plt.scatter(x, y, **kwargs)
+
+    # # prepending and appending 0.0 values to close the shape for filling
+    # x[x.index[-1] + 1] = 0.0
+    # y[y.index[-1] + 1] = 0.0
+    # b = [0.00]
+    # b[1:] = x
+    # x = pd.Series(b)
+    # c = [0.00]
+    # c[1:] = y
+    # y = pd.Series(c)
+
+    plt.scatter(x, y, marker='.')
+    ax = plt.gca()
+    d = scipy.zeros(len(y))
+    ax.fill_between(x, y, where=y >= d, interpolate=True, facecolor='lightcyan', alpha=0.7)
+    ax.fill_between(x, y, where=y <= d, interpolate=True, facecolor='peachpuff', alpha=0.7)
+
+
+def facet_scatter_pos(x, y, **kwargs):
+    """Draw scatterplot with point colors from a faceted DataFrame columns."""
+    kwargs.pop("color")
+    plt.scatter(x, y, marker='.')
+    ax = plt.gca()
+    d = scipy.zeros(len(y))
+    ax.fill_betweeny(x, y, where=0 - y > 0 , interpolate=True, facecolor='lightcyan', alpha=0.7)
+
+
+def facet_scatter_neg(x, y, **kwargs):
+    """Draw scatterplot with point colors from a faceted DataFrame columns."""
+    kwargs.pop("color")
+    plt.scatter(x, y, marker='.')
+    ax = plt.gca()
+    d = scipy.zeros(len(y))
+    ax.fill_betweeny(x, y, where=0 - y < 0, interpolate=True, facecolor='peachpuff', alpha=0.7)
 
 
 if __name__ == '__main__':
@@ -32,12 +66,11 @@ if __name__ == '__main__':
 
         try:
             df = pd.read_csv(os.path.join(data_path + file_csv), sep=';',
-                            converters={'SECTION AREA [Ų]': lambda x: (x.replace(",", ".")),
-                                        'TIME STEP': lambda x: (x.replace(",", ".")),
-                                        'OFFSET': lambda x: (x.replace(",", "."))})
+                             converters={'SECTION AREA [Ų]': lambda x: (x.replace(",", ".")),
+                                         'TIME STEP': lambda x: (x.replace(",", ".")),
+                                         'OFFSET': lambda x: (x.replace(",", "."))})
         except pd.errors.EmptyDataError as err:
             continue
-
 
         types_dict = {'SECTION AREA [Ų]': float, 'TIME STEP': float, 'OFFSET': float}
         for col, col_type in types_dict.items():
@@ -49,8 +82,11 @@ if __name__ == '__main__':
         # write to csv
         # df_filtered = train.loc[train['TIME STEP'].isin([0.0, 4.0, 6.0, 8.0])]
         df_filtered = train.loc[train['TIME STEP'].isin([0.0])]
-        df_filtered['struct'] = '_'.join(file_csv.split('_')[:3])
-        df_filtered.to_csv('filtered_{}.csv'.format('_'.join(file_csv.split('_')[:3])))
+        df_filtered['struct'] = ''.join(file_csv.split('_')[2])
+        df_filtered['mode'] = int(file_csv.split('_')[3][1:])
+        df_filtered['amplitude'] = float(file_csv.split('_')[4])
+        # df_filtered.to_csv('filtered_{}.csv'.format('_'.join(file_csv.split('_')[:3])))
+
         pd_dict['_'.join(file_csv.split('_')[:3])] = df_filtered
 
         # h = sns.FacetGrid(train, col="OFFSET", palette='seismic', sharey=False, sharex=True, col_wrap=6, height=2,
@@ -65,19 +101,48 @@ if __name__ == '__main__':
     all = pd.concat(pd_dict.values(), axis=0, ignore_index=True)
     all.to_csv('nats_tunnel_area.csv')
 
+
+    # res = all.groupby(['struct', 'mode']).apply(
+    #     lambda g: g['SECTION AREA [Ų]'] -
+    #               g[g.amplitude == 0.00]["SECTION AREA [Ų]"].values[0])
+    # all['diff'] = res.reset_index(drop=True)
+
+    def subspace(row, df):
+        mode = row['mode']
+        struct = row['struct']
+        offset = row['OFFSET']
+        air0 = df.query("mode == @mode and struct == @struct and amplitude == 0 and OFFSET == @offset")[
+            "SECTION AREA [Ų]"]
+        if air0.empty:
+            airdiff = np.nan
+        else:
+            airdiff = row['SECTION AREA [Ų]'] - list(air0)[0]
+        return airdiff
+
+
+    all['diff'] = all.apply(lambda r: subspace(r, all), axis=1)
+
     col_list = list(set(all['struct'].tolist()))
     col_list.sort()
 
-    h = sns.FacetGrid(all, col="struct", sharey='row', sharex=True, col_wrap=6, height=3,
-                      aspect=1, hue="OFFSET", col_order=col_list)
-    sns.color_palette("BuGn_r")
-    # all["OFFSET"] = all["OFFSET"].astype(str)
+    for mode in [7, 8, 9, 10, 11, 12]:
+        all_mode = all.loc[(all['mode'] == mode)]
+        all_mode_0 = all_mode.loc[(all_mode['amplitude'] != 0.00)]
 
-    h.map(sns.lineplot, "TIME STEP", "SECTION AREA [Ų]")
-    h.map(sns.scatterplot, "TIME STEP", "SECTION AREA [Ų]", data=all)
+        h = sns.FacetGrid(all_mode_0, col="struct", sharex=True, col_wrap=3, height=2,
+                          aspect=1, hue="amplitude", col_order=col_list)
+        sns.color_palette("BuGn_r")
+        # all["OFFSET"] = all["OFFSET"].astype(str)
 
-    plt.legend()
+        # h.map(sns.lineplot, "OFFSET", "SECTION AREA [Ų]")
+        # h.map(sns.scatterplot, "OFFSET", "SECTION AREA [Ų]", data=all_mode)
+        h.map(sns.lineplot, "OFFSET", "diff")
+        h.map(facet_scatter, "OFFSET", "diff")
+        # h.map(facet_scatter_pos, "OFFSET", "diff")
+        # h.map(facet_scatter_neg, "OFFSET", "diff")
 
-    plt.subplots_adjust(top=0.9)
-    h.fig.suptitle("all: Area=f(time step)")
-    plt.show()
+        plt.legend()
+
+        plt.subplots_adjust(top=0.9)
+        h.fig.suptitle(str(mode))
+        plt.show()
